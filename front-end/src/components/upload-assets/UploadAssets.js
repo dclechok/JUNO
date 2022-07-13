09d2031830192374ccce3c53d92464cb0378fc36
dclechok <dclechok@gmail.com>
2022-07-13 09:36:46 -0400

    added active button sty ling to upload assets page

--- front-end/src/components/upload-assets/UploadAssets.js
+++ front-end/src/components/upload-assets/UploadAssets.js
@@ -6,9 +6,15 @@ import SingleUpload from './SingleUpload.js';
 function UploadAssets({ assetList, setLoadAssets, loadAssets }){
 
     const [toggleUpload, setToggleUpload] = useState('');
+    const defaultButtonState = { 
+      singleUpload: "button-link button-link-font",
+      bulkUpload: "button-link button-link-font"
+    };
+    const [buttonState, setButtonState] = useState(defaultButtonState);
 
     const onClickHandler = (e) => {
         const { id = '' } = e.currentTarget;
+        setButtonState({...defaultButtonState, [id]: "active-button-link button-link-font"});
         setToggleUpload(id);
     };
 
@@ -17,15 +23,15 @@ function UploadAssets({ assetList, setLoadAssets, loadAssets }){
       <h1>Upload Assets</h1>
         <header className="upload-options">
           <div>
-            <h2>[<button id="bulk-upload" onClick={onClickHandler}>Bulk Asset Upload</button>]</h2>
+            <h2>[<button className={buttonState.bulkUpload} id="bulkUpload" onClick={onClickHandler}>Bulk Asset Upload</button>]</h2>
           </div>
           <div>
-            <h2>[<button id="single-upload" onClick={onClickHandler}>Single Asset Upload</button>]</h2>
+            <h2>[<button className={buttonState.singleUpload} id="singleUpload" onClick={onClickHandler}>Single Asset Upload</button>]</h2>
           </div>
         </header>
 
-        {toggleUpload === 'bulk-upload' && <BulkUpload assetList={assetList} setLoadAssets={setLoadAssets} loadAssets={loadAssets} />}
-        {toggleUpload === 'single-upload' && <SingleUpload assetList={assetList} setLoadAssets={setLoadAssets} loadAssets={loadAssets} />}
+        {toggleUpload === 'bulkUpload' && <BulkUpload assetList={assetList} setLoadAssets={setLoadAssets} loadAssets={loadAssets} />}
+        {toggleUpload === 'singleUpload' && <SingleUpload assetList={assetList} setLoadAssets={setLoadAssets} loadAssets={loadAssets} />}
         </div>
       )
 }
