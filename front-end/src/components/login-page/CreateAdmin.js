f6347cc2828ac020de2c864051f367d2c4f09763
dclechok <dclechok@gmail.com>
2022-07-18 14:09:57 -0400

    renders create admin component when no admins or users exist in DB, and once account is created, we render the normal login component

--- front-end/src/components/login-page/CreateAdmin.js
+++ front-end/src/components/login-page/CreateAdmin.js
@@ -1,62 +1,97 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
+import { createUser } from "../../utils/api";
+const bcrypt = require("bcryptjs");
 
-import { createUser } from '../../utils/api';
-
-function CreateAdmin() {
-  
+function CreateAdmin({ setCreateAdmin }) {
   const defaultUserData = {
-    username: '',
-    password: '',
-    email: '', 
-    name: '', 
-    access_level: 'admin'
+    username: "",
+    hash: "",
+    email: "",
+    name: "",
+    access_level: "admin",
   };
-  const [createUser, setCreateUser] = useState(defaultUserData);
-  
+  const [userData, setUserData] = useState(defaultUserData);
+  const [validatedUserData, setValidatedUserData] = useState(true); //to be used for future validation
+  const [newAdmin, setNewAdmin] = useState(null);
+
   const changeHandler = (e) => {
     const { id, value } = e.currentTarget;
-    setCreateUser({...createUser, [id]: value });
+    setUserData({ ...userData, [id]: value });
   };
 
   const submitHandler = (e) => {
-    //validate date
+    //frontend validate user data here before submitting
     e.preventDefault();
-  };
+    const newHash = bcrypt.hashSync(userData.hash, 15);
+    async function createNewAdmin(){
+      setNewAdmin(await createUser({...userData, hash: newHash}));
+    }
+    createNewAdmin();
 
+  };
+  useEffect(() => {
+    if(newAdmin) setCreateAdmin(false);
+  }, [setNewAdmin, newAdmin]);
+  console.log(newAdmin);
   return (
     <>
-    <div className="login-header">
-      <h3>No User/Admin Account Exists!</h3>
-      <h5><span style={{textAlign: "center"}}>One must be created before proceeding.</span></h5>
+      <div className="login-header">
+        <h3>No User/Admin Account Exists!</h3>
+        <h5>
+          <span style={{ textAlign: "center" }}>
+            One must be created before proceeding.
+          </span>
+        </h5>
       </div>
       <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
         <br />
         <br />
-        <input id="username" type="text" onChange={changeHandler} value={createUser.username}/>
+        <input
+          id="username"
+          type="text"
+          onChange={changeHandler}
+          value={userData.username}
+        />
         <br />
         <br />
-        <label htmlFor="password">Password</label>
+        <label htmlFor="hash">Password</label>
         <br />
         <br />
-        <input id="password" type="password" onChange={changeHandler}  value={createUser.password}/>
+        <input
+          id="hash"
+          type="password"
+          onChange={changeHandler}
+          value={userData.hash}
+        />
         <br />
         <br />
         <label htmlFor="email">Email</label>
         <br />
         <br />
-        <input id="email" type="text" onChange={changeHandler} value={createUser.email} />
+        <input
+          id="email"
+          type="text"
+          onChange={changeHandler}
+          value={userData.email}
+        />
         <br />
         <br />
         <label htmlFor="name">Name (First Last)</label>
         <br />
         <br />
-        <input id="name" type="text" onChange={changeHandler} value={createUser.name}/>
+        <input
+          id="name"
+          type="text"
+          onChange={changeHandler}
+          value={userData.name}
+        />
         <div className="signin-button">
-        <button className="create-admin" type="submit">Create</button>
-      </div>
+          <button className="create-admin" type="submit">
+            Create
+          </button>
+        </div>
       </form>
-
     </>
   );
 }
--- front-end/src/components/login-page/Login.js
+++ front-end/src/components/login-page/Login.js
@@ -8,7 +8,7 @@ import CreateAdmin from "./CreateAdmin";
 import { getUsers } from "../../utils/api";
 
 function Login() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(null);
   const [createAdmin, setCreateAdmin] = useState(false);
 
   useEffect(() => {
@@ -16,48 +16,55 @@ function Login() {
     async function loadUsers() {
       setUsers(await getUsers());
     }
-    if(users.length === 0 || !users.find(user => user.access_level === "admin")){
-      //no users or administrators exist in the system
-      setCreateAdmin(true);
-    }
-
+    loadUsers();
     return abortController.abort();
   }, []);
 
   useEffect(() => {
-    // console.log("create admin", createAdmin);
-  }, [createAdmin, setCreateAdmin]);
+    if (users) {
+      if (
+        users.length === 0 ||
+        !users.find((user) => user.access_level === "admin")
+      ) {
+        //no users or administrators exist in the system
+        setCreateAdmin(true);
+      }
+    }
+  }, [users, setUsers]);
 
   return (
     <main>
       <div className="login-container">
         <div className="title-header">
-        <h1>
-          JUN<span style={{ color: "rgb(255, 239, 138)" }}>O</span>
-        </h1>
-        <h3>Asset Management System</h3>
-        <h4>Powered by Mawson Infrastructure Group</h4>
+          <h1>
+            JUN<span style={{ color: "rgb(255, 239, 138)" }}>O</span>
+          </h1>
+          <h3>Asset Management System</h3>
+          <h4>Powered by Mawson Infrastructure Group</h4>
         </div>
         <div className="login-form-container">
-        {createAdmin ? 
-        <CreateAdmin /> :<>
-          <h2>Sign In</h2>
-            <form>
-              <label htmlFor="user">Username</label>
-              <br />
-              <br />
-              <input id="user" type="text" />
-              <br />
-              <br />
-              <label htmlFor="pass">Password</label>
-              <br />
-              <br />
-              <input id="pass" type="password" />
-            </form>
-          <div className="signin-button">
-            <button>Login</button>
-          </div></>
-          }
+          {createAdmin ? (
+            <CreateAdmin setCreateAdmin={setCreateAdmin}/>
+          ) : (
+            <>
+              <h2>Sign In</h2>
+              <form>
+                <label htmlFor="user">Username</label>
+                <br />
+                <br />
+                <input id="user" type="text" />
+                <br />
+                <br />
+                <label htmlFor="pass">Password</label>
+                <br />
+                <br />
+                <input id="pass" type="password" />
+              </form>
+              <div className="signin-button">
+                <button>Login</button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </main>
