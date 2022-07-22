f327b65a6cedd7c41efd74b7448305b07044f1cc
dclechok <dclechok@gmail.com>
2022-07-22 13:55:08 -0400

    made 'create user' ui with radio buttons

--- front-end/src/components/admin-panel/manage-users/CreateUser.js
+++ front-end/src/components/admin-panel/manage-users/CreateUser.js
@@ -1,10 +1,64 @@
-function CreateUser(){
+import "./CreateUser.css";
 
-    return(
-        <>
-        
-        </>
-    );
+function CreateUser() {
+  const submitHandler = () => {};
+
+  return (
+    <section className="create-user-container upload-container-style">
+      <h4>Create User</h4>
+      <form
+        className="form-container create-user-form"
+        onSubmit={submitHandler}
+      >
+        <fieldset>
+          <legend>User Access Level</legend>
+          <div className="radio-buttons-container">
+            <div className="flex-header">
+              <label htmlFor="admin">Admin</label>
+              <label htmlFor="engineer">Engineer</label>
+              <label htmlFor="analyst">Analyst</label>
+            </div>
+              <div className="flex-buttons">
+                <input
+                  type="radio"
+                  id="admin"
+                  name="access_level"
+                  value="admin"
+                />
+
+                <input
+                  type="radio"
+                  id="engineer"
+                  name="access_level"
+                  value="engineer"
+                />
+
+                <input
+                  type="radio"
+                  id="analyst"
+                  name="access_level"
+                  value="analyst"
+                />
+              </div>
+            </div>
+        </fieldset>
+
+        <div className="create-space">
+          <label htmlFor="name">Name ("John Doe")</label>
+          <input type="text" id="name" name="name" />
+
+          <label htmlFor="username">Username</label>
+          <input type="username" id="username" name="username" />
+
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password" name="password" />
+          <label htmlFor="email">Email Address</label>
+          <input type="text" id="email" name="email" />
+        </div>
+        <div className="fix-button"><button className="submit-single-asset">Create User</button></div>
+      </form>
+    </section>
+  );
 }
 
 export default CreateUser;
--- front-end/src/components/admin-panel/manage-users/ManageUsers.js
+++ front-end/src/components/admin-panel/manage-users/ManageUsers.js
@@ -1,52 +1,73 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 //components
-import LoaderSpinner from '../../LoaderSpinner';
-import ViewUsers from './ViewUsers';
-import CreateUsers from './CreateUser';
+import LoaderSpinner from "../../LoaderSpinner";
+import ViewUsers from "./ViewUsers";
+import CreateUser from "./CreateUser";
 //utils
-import { getUsers } from '../../../utils/api';
-import dateFormatter from '../../../utils/dateFormatter';
+import { getUsers } from "../../../utils/api";
 
-function ManageUsers({ accountLogged }){
+function ManageUsers({ accountLogged }) {
+  const [users, setUsers] = useState(null);
+  const defaultButtonStyle = {
+    view: "button-link",
+    create: "button-link",
+  };
+  const [buttonStyle, setButtonStyle] = useState(defaultButtonStyle);
+  const [viewOrCreate, setViewOrCreate] = useState("view");
 
-    const [users, setUsers] = useState(null);
-    const defaultButtonStyle = {
-      view: "button-link",
-      create: "button-link"
-    };
-    const [buttonStyle, setButtonStyle] = useState(defaultButtonStyle);
-    const [viewOrCreate, setViewOrCreate] = useState('view');
+  useEffect(() => {
+    async function populateUsers() {
+      setUsers(await getUsers());
+    }
+    populateUsers();
+  }, []);
 
-    useEffect(() => {
-        async function populateUsers(){
-            setUsers(await getUsers());    
-        }
-        populateUsers();
-    }, []);
+  useEffect(() => {
+    setButtonStyle({ ...buttonStyle, [viewOrCreate]: "active-button-link" }); //default to View
+  }, [viewOrCreate, setViewOrCreate]);
 
-    useEffect(() => {
-      setButtonStyle({...buttonStyle, [viewOrCreate]: 'active-button-link'}); //default to View
-    }, [viewOrCreate, setViewOrCreate]);
+  const handleClick = (e) => {
+    e.preventDefault();
+    const { id } = e.currentTarget;
+    setViewOrCreate(id);
+    setButtonStyle({ ...defaultButtonStyle, [id]: "active-button-link" });
+  };
 
-    const handleClick = (e) => {
-      e.preventDefault();
-      const { id } = e.currentTarget;
-      setViewOrCreate(id);
-      setButtonStyle({...defaultButtonStyle, [id]: 'active-button-link'});
-    };
-
-    return (
-        <>
-          {users && users.length !== 0 ? (
-          <div>
-        <span style={{color: "black"}}>[<button className={buttonStyle.view} id="view" onClick={handleClick} >View Users</button>]</span>&nbsp;
-        <span style={{color: "black"}}>[<button className={buttonStyle.create} id="create" onClick={handleClick} >Create User</button>]</span>
-        {viewOrCreate === "view" && <ViewUsers users={users} />}
+  return (
+    <>
+      {users && users.length !== 0 ? (
+        <div>
+          <span style={{ color: "black" }}>
+            [
+            <button
+              className={buttonStyle.view}
+              id="view"
+              onClick={handleClick}
+            >
+              View Users
+            </button>
+            ]
+          </span>
+          &nbsp;
+          <span style={{ color: "black" }}>
+            [
+            <button
+              className={buttonStyle.create}
+              id="create"
+              onClick={handleClick}
+            >
+              Create User
+            </button>
+            ]
+          </span>
+          {viewOrCreate === "view" && <ViewUsers users={users} />}
+          {viewOrCreate === "create" && <CreateUser />}
         </div>
-        ) : <LoaderSpinner />}
-        </>
-    );
-
+      ) : (
+        <LoaderSpinner width={45} height={45} message={"User Data"}/>
+      )}
+    </>
+  );
 }
 
 export default ManageUsers;
