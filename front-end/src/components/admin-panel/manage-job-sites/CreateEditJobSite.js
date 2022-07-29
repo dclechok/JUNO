19100415e27e6b114af203bafdd2bc10bb8bacef
dclechok <dclechok@gmail.com>
2022-07-29 14:15:01 -0400

    conditional rendering of form placeholders

--- front-end/src/components/admin-panel/manage-job-sites/CreateEditJobSite.js
+++ front-end/src/components/admin-panel/manage-job-sites/CreateEditJobSite.js
@@ -2,44 +2,76 @@ import { useEffect, useState } from "react";
 
 import LoaderSpinner from "../../LoaderSpinner";
 //utils
-import { getJobSite, updateJobSite } from "../../../utils/api";
+import { getJobSite, updateJobSite, createJobSite } from "../../../utils/api";
+import generateHistoryKey from "../../../utils/generateHistoryKey";
 
-function CreateEditJobSite({ accountLogged, setViewOrCreate, viewOrCreate, jobSiteID = '' }) {
-  const [oldSiteData, setOldSiteData] = useState(null);
-  const [newSiteData, setNewSiteData] = useState(null);
+function CreateEditJobSite({
+  accountLogged,
+  setViewOrCreate,
+  viewOrCreate,
+  jobSiteID = "",
+}) {
+  const newHistoryKey = generateHistoryKey(); //generate unique history key ("action_key")
+  const newDate = new Date();
+
+  const [oldSiteData, setOldSiteData] = useState({});
+  const [newSiteData, setNewSiteData] = useState({});
   const [success, setSuccess] = useState(null);
 
+  const defaultJobSite = {
+    physical_site_name: "",
+    physical_site_loc: "",
+    created_by: accountLogged.account[0].name,
+    site_code: '',
+    status: "Active",
+    first_octet: '',
+    history: [
+      //the date of creation can be found by searching history_log via key of historical item
+      {
+        action_taken: "Create Job Site",
+        action_by: accountLogged.account[0].name,
+        action_by_id: accountLogged.account[0].user_id,
+        action_key: newHistoryKey, //generate unique history key ("action_key")
+        action_date: newDate,
+        action_comment: "Job Site Creation",
+      },
+    ],
+  };
+
   useEffect(() => {
+    //get old site data if we are editing
     async function grabJobSite() {
-      setOldSiteData(...await getJobSite(jobSiteID));
-      if(oldSiteData) setNewSiteData(oldSiteData);
+      setOldSiteData(...(await getJobSite(jobSiteID)));
+      if (oldSiteData) setNewSiteData(oldSiteData); //editing old data
     }
-    if(jobSiteID) grabJobSite();
-  }, []);
-  console.log(viewOrCreate)
+    if (jobSiteID) grabJobSite();
+    if (viewOrCreate === "create") setOldSiteData(defaultJobSite);
+  }, [viewOrCreate, setViewOrCreate]);
+
   const changeHandler = (e) => {
     const { id, value } = e.currentTarget;
     setNewSiteData({ ...newSiteData, [id]: value });
   };
   const submitHandler = (e) => {
     e.preventDefault();
-    async function makeJobSiteUpdate(){
+    async function makeJobSite() {
+      if (viewOrCreate === "edit")
         setSuccess(await updateJobSite(newSiteData, accountLogged));
+      else if (viewOrCreate === "create")
+        setSuccess(await createJobSite(newSiteData, accountLogged));
     }
-    makeJobSiteUpdate();
+    makeJobSite();
   };
 
   useEffect(() => {
-    if(oldSiteData) setNewSiteData(oldSiteData);
-  }, [oldSiteData]);
-
-  useEffect(() => {
-    if(success) setViewOrCreate('view');
+    if (success) setViewOrCreate("view");
   }, [success]);
-  
+
   return (
     <section className="create-user-container upload-container-style">
-      <h4>{viewOrCreate.charAt(0).toUpperCase() + viewOrCreate.slice(1)} Job Site</h4>
+      <h4>
+        {viewOrCreate.charAt(0).toUpperCase() + viewOrCreate.slice(1)} Job Site
+      </h4>
       {newSiteData ? (
         <form
           className="form-container create-user-form"
@@ -54,32 +86,33 @@ function CreateEditJobSite({ accountLogged, setViewOrCreate, viewOrCreate, jobSi
               id="physical_site_name"
               name="physical_site_name"
               onChange={changeHandler}
-              placeholder={oldSiteData.physical_site_name}
+              placeholder={viewOrCreate === 'edit' ? oldSiteData.physical_site_name : defaultJobSite.physical_site_name}
             />
 
-            <label htmlFor="site_code">
-              Site Code (ex. "MLP/P1")
-            </label>
+            <label htmlFor="site_code">Site Code (ex. "MLP/P1")</label>
             <input
               type="text"
               id="site_code"
               name="site_code"
               onChange={changeHandler}
-              placeholder={oldSiteData.site_code}
+              placeholder={viewOrCreate === 'edit' ? oldSiteData.site_code : defaultJobSite.site_code}
             />
-                        <label htmlFor="first_octet">
-            Site IP First Octet (ex. "10".x.x.x)
+            <label htmlFor="first_octet">
+              Site IP First Octet (ex. "10".x.x.x)
             </label>
             <input
               type="text"
               id="first_octet"
               name="first_octet"
               onChange={changeHandler}
-              placeholder={oldSiteData.first_octet}
+              placeholder={viewOrCreate === 'edit' ? oldSiteData.first_octet : defaultJobSite.first_octet}
             />
           </div>
           <div className="fix-button">
-            <button className="submit-single-asset">{viewOrCreate.charAt(0).toUpperCase() + viewOrCreate.slice(1)} Job Site</button>
+            <button className="submit-single-asset">
+              {viewOrCreate.charAt(0).toUpperCase() + viewOrCreate.slice(1)} Job
+              Site
+            </button>
           </div>
         </form>
       ) : (
---
+++ front-end/src/components/admin-panel/manage-job-sites/CreateSite.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+
+//utils
+import generateHistoryKey from "../../../utils/generateHistoryKey";
+import { createJobSite } from "../../../utils/api";
+import LoaderSpinner from "../../LoaderSpinner";
+
+function CreateSite({ accountLogged, setViewOrCreate }) {
+  const newHistoryKey = generateHistoryKey(); //generate unique history key ("action_key")
+  const newDate = new Date();
+  const defaultJobSite = {
+    physical_site_name: "",
+    physical_site_loc: "",
+    created_by: accountLogged.account[0].name,
+    status: "Active",
+    history: [ //the date of creation can be found by searching history_log via key of historical item
+      {
+        action_taken: "Create Job Site",
+        action_by: accountLogged.account[0].name,
+        action_by_id: accountLogged.account[0].user_id,
+        action_key: newHistoryKey, //generate unique history key ("action_key")
+        action_date: newDate,
+        action_comment: "Job Site Creation"
+      }
+    ]
+  };
+
+  const [newJobSite, setNewJobSite] = useState(defaultJobSite);
+  const [createButtonDisabled, setCreateButtonDisabled] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    async function createNewJobSite(){
+      await createJobSite(newJobSite);
+      setViewOrCreate("view");
+    }
+    createNewJobSite();
+    setCreateButtonDisabled(true);
+  };
+
+  const changeHandler = (e) => {
+    const { id, value } = e.currentTarget;
+    setNewJobSite({ ...newJobSite, [id]: value });
+  };
+  
+  return (
+    <section className="upload-container-style">
+      <h4>Create Job Site</h4>
+      <>
+      {!createButtonDisabled ? 
+        <form className="form-container" onSubmit={submitHandler}>
+          <div className="create-space">
+            <label htmlFor="physical_site_name">
+              Job Site (ex. "Midland, PA")
+            </label>
+            <input
+              id="physical_site_name"
+              type="text"
+              value={newJobSite.physical_site_name}
+              onChange={changeHandler}
+            />
+            <br />
+          </div>
+          <div className="create-space">
+            <label htmlFor="physical_site_loc">Site ID (ex. "MLP-PA")</label>
+            <input
+              id="physical_site_loc"
+              type="text"
+              value={newJobSite.physical_site_loc}
+              onChange={changeHandler}
+            />
+            <br />
+          </div>
+          
+          <div className="fix-button">
+            <button className="submit-single-asset" type="submit" disabled={createButtonDisabled}>
+              Upload Job Site
+            </button>
+          </div>
+          
+        </form>
+        : <LoaderSpinner width={45} height={45} message="Job Sites" />}
+      </>
+    </section>
+  );
+}
+
+export default CreateSite;
