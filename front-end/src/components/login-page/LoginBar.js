8c200bb8301a31f54b9fccb6d42851e2c158841a
Daniel C. Lechok <dclechok@gmail.com>
2022-09-30 14:53:38 -0400

    fixed styling of sign out button

--- front-end/src/components/login-page/LoginBar.js
+++ front-end/src/components/login-page/LoginBar.js
@@ -5,34 +5,26 @@ import signout from '../../images/signout.png';
 //Active Directory
 import { useMsal } from "@azure/msal-react";
 
-function LoginBar({ currentAcct, setCurrentAcct }){
-
-    const navigate = useNavigate();
-    const { instance } = useMsal();
-    const handleLogout = (e) => {
-        const { id } = e.currentTarget;
-        if(id === "logout"){ //clear local storage, redirect to entrypoint
-          instance.logoutRedirect({
-            account: instance.getActiveAccount(),
-            postLogoutRedirectUri: "/"
-          });
-          if(!instance.getActiveAccount()) setCurrentAcct(null);
-        }
-      };
+function LoginBar({ currentAcct, setCurrentAcct }) {
   
-      const handleClick = (e) => {
-        const { id } = e.currentTarget;
-        if(id === "user-panel"){
-          navigate('/user-panel');
-        }
-      };
+  const { instance } = useMsal();
+  const handleLogout = (e) => {
+    const { id } = e.currentTarget;
+    if (id === "logout") { //clear local storage, redirect to entrypoint
+      instance.logoutRedirect({
+        account: instance.getActiveAccount(),
+        postLogoutRedirectUri: "/"
+      });
+      if (!instance.getActiveAccount()) setCurrentAcct(null);
+    }
+  };
 
-    return (
+  return (
     <div className='logged-in'>
       <p>{currentAcct.name}</p>
-        <button className="power-btn" id="logout" onClick={handleLogout}><img className="power-btn" src={signout} alt="sign out" /></button>
-        </div>
-    );
+      <button className="power-btn" id="logout" onClick={handleLogout}><img className="power-btn" src={signout} alt="sign out" /></button>
+    </div>
+  );
 }
 
 export default LoginBar;
